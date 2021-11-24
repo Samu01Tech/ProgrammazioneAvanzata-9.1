3d9af974f8d3f0f1e6f20d585726aab56c02e08d
Samuele Mazzei <59342479+Samu01Tech@users.noreply.github.com>
2021-11-24 14:58:23 +0000

    altre modalità di inserimento map[i]

--- main.cpp
+++ main.cpp
@@ -7,8 +7,14 @@ int main() {
     map<int, string> mi;
 
     mi.insert(pair<int,string>(7, string("Giovanni")));
-    mi.insert(pair<int,string>(3, string("Luca")));
+    mi.insert(pair<int,string>(1, string("Luca")));
+    
+    //se c'è non fa nulla
     mi.insert(pair<int,string>(7, string("Boh")));
+    //se c'è sovrascrive
+    mi[7] = "Boh";
+    //se non c'è
+    mi[5] = "Maria";
 
     map<int, string>::iterator iter;
     for(iter = mi.begin(); iter != mi.end(); ++iter){
