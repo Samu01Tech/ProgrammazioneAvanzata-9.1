9c2f528fa14474bfd9e36f88818ff3357d0bebe1
Samuele Mazzei <59342479+Samu01Tech@users.noreply.github.com>
2021-11-24 14:56:04 +0000

    Insert pair(chiave singola) e stampa

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <map>
+#include <string>
 using namespace std;
 
 int main() {
-  cout << "Hello World!\n";
+    map<int, string> mi;
+
+    mi.insert(pair<int,string>(7, string("Giovanni")));
+    mi.insert(pair<int,string>(3, string("Luca")));
+    mi.insert(pair<int,string>(7, string("Boh")));
+
+    map<int, string>::iterator iter;
+    for(iter = mi.begin(); iter != mi.end(); ++iter){
+        cout << iter->first << " " << iter->second << endl;
+    }
+
 } 
